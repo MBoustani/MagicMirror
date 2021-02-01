6d24e3083d541b35ad0e411bee5d7ff98cec104d
Mazi Boustani <mboustani@beyond.ai>
2021-02-01 09:56:21 -0800

    adding readme and minor modifications

---
+++ README.md
@@ -0,0 +1,21 @@
+# Magic Mirror 
+
+
+ ### add your openweathermap API Key and city id
+### to get the API key go to link: https://home.openweathermap.org/api_keys
+### openweathermap_API_Key = ""
+
+### to find the city ID download this file and search for you city, grab the id and put in city_id varaible
+### http://bulk.openweathermap.org/sample/city.list.json.gz 
+
+### For Los Angeles 
+### city_id = "5368361"
+
+## To Run the website
+
+## python app.py
+
+
+Open the browser and go to the link: http://localhost:5000/
+
+Wait around 30 seconds for all information to show up
--- app.py
+++ app.py
@@ -3,9 +3,21 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    # add your openweathermap API Key and city id
+    # to get the API key go to link: https://home.openweathermap.org/api_keys
+    openweathermap_API_Key = ""
+
+    # to find the city ID download this file and search for you city, grab the id and put in city_id varaible
+    # http://bulk.openweathermap.org/sample/city.list.json.gz
+    city_id = ""
+
+    return render_template('index.html',
+                            city_id=city_id,
+                            openweathermap_API_Key=openweathermap_API_Key)
 
 if __name__ == '__main__':
     app.debug = True
